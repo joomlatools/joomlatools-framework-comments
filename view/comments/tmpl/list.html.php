b87591b3456ebe630cae7ab76d363127440c24dd
Jebb Domingo <jebb@wizmediateam.com>
2016-03-09 18:30:22 +0800

    Re: #6 Added anchor tag to make each comment bookmark-able

--- view/comments/tmpl/list.html.php
+++ view/comments/tmpl/list.html.php
@@ -12,6 +12,7 @@
     <? foreach($comments as $comment) :?>
         <div class="comment">
             <div class="comment-header">
+                <a name="comment-<?= $comment->id ?>"></a>
                 <?= $comment->created_by == object('user')->id ? translate('You') : $comment->created_by_name ?>&nbsp;<?= translate('wrote') ?>
                 <time datetime="<?= $comment->created_on ?>" pubdate><?= helper('date.humanize', array('date' => $comment->created_on)) ?></time>
             </div>
