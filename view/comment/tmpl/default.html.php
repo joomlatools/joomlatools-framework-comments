60e273a3c1b20013b61d37b63f8956d6872f5409
Ercan Ozkaya <ercan@timble.net>
2016-03-25 13:12:57 +0200

    Change repo name

--- view/comment/tmpl/default.html.php
+++ view/comment/tmpl/default.html.php
@@ -4,7 +4,7 @@
  *
  * @copyright   Copyright (C) 2011 - 2015 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link        http://github.com/nooku/nooku-comments for the canonical source repository
+ * @link        http://github.com/joomlatools/joomlatools-framework-comments for the canonical source repository
  */
 
 $route = ($state->route) ? "{$state->route}&row={$state->row}&table={$state->table}" : "row={$state->row}&table={$state->table}";
